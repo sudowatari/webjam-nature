ceeb50733b2514cda6144b97d011895b127c46c8
Sudowatari <hamptonvalia@icloud.com>
2021-08-20 00:12:28 -0700

    Twitch Stream August 19 2021

--- README.md
+++ README.md
@@ -10,3 +10,16 @@ Difficulty level: Beginner (Only make one page, no need for functionality either
 - Setup skeleton and main objectives
 - HTML/CSS/Basic javascript in place
 - Use Unsplash for images (maybe prep images for next stream)
+
+<h3>Aug 19 2021 - Twitch stream</h3>
+- Finished finding images and added to cards
+- Setup the modals
+- Added google fonts
+- changed colour theme
+- 
+
+<h3> For last stream before due date:</h3>
+- Add text to modals for each flower (decide beforehand what type of description to add)
+- add javascript to each button and modal.
+- think of any thing else that you can add to make it more better/challenging
+- add the attributes somewhere
--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 //variable
 
-const sunflowerBtn = document.querySelector('#click1');
-const SFmodal = document.querySelector('.SFmodal');
+const sunflowerBtn = document.querySelector('#click1'),
+        daisyBtn = document.querySelector('#click2');
+const SFmodal = document.querySelector('.SFmodal'),
+        daisyModal = document.querySelector('.daisymodal');
 const close = document.querySelector('.close');
 
+//functions
+
+function openModal(e) {
+    e.preventDefault();
+    SFmodal.style.display = 'block';
+}
+
+function closeModal() {
+    SFmodal.style.display = 'none';
+}
+
+//close event
+close.addEventListener('click', closeModal);
+
+//cards
+    //sunflower
+sunflowerBtn.addEventListener('click', openModal);
+SFmodal.addEventListener('click', closeModal);
+
+    //daisy
+daisyBtn.addEventListener('click', openModal);
+daisyModal.addEventListener('click', closeModal);
